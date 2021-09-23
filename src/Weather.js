14ab78f76f9cfc3e00c7371a8007303032a208f8
batesmichelle23 <batesmichelle23@gmail.com>
2021-09-23 13:47:12 -0500

    Transferred CSS/Weather app

--- src/Weather.js
+++ src/Weather.js
@@ -11,77 +11,15 @@ export default function Weather() {
         <span className="icon">
           <i className="fas fa-sun"></i>
         </span>
-      </p>
-      <div className="forecast">
-        <div className="row" id="days">
-          <div className="col" id="sun">
-            Sun
+          </p>
+
+          <div className="container">
+              <div className="row">
+                  <div className="col">testing</div>
+                  <div className="col">testing</div>
+              </div>
+              
           </div>
-          <div className="col" id="mon">
-            Mon
           </div>
-          <div className="col" id="tue">
-            Tue
-          </div>
-          <div className="col" id="wed">
-            Wed
-          </div>
-          <div className="col" id="thu">
-            Thu
-          </div>
-        </div>
-        <div className="row" id="temps">
-          <div className="col" id="tempDay1">
-            72&#176;F
-          </div>
-          <div className="col" id="tempDay2">
-            79&#176;F
-          </div>
-          <div className="col" id="tempDay3">
-            65&#176;F
-          </div>
-          <div className="col" id="tempDay4">
-            83&#176;F
-          </div>
-          <div className="col" id="tempDay5">
-            80&#176;F
-          </div>
-        </div>
-        <div className="row" id="icons">
-          <div className="col" id="iconDay1">
-            <i className="fas fa-cloud-sun"></i>
-          </div>
-          <div className="col" id="iconDay2">
-            <i className="fas fa-cloud-sun"></i>
-          </div>
-          <div className="col" id="iconDay3">
-            <i className="fas fa-cloud-sun"></i>
-          </div>
-          <div className="col" id="iconDay4">
-            <i className="fas fa-cloud-sun"></i>
-          </div>
-          <div className="col" id="iconDay5">
-            <i className="fas fa-cloud-sun"></i>
-          </div>
-        </div>
-        <div className="row" id="feelsLike">
-          <div className="col" id="feelsLikeDay1">
-            Sunny
-          </div>
-          <div className="col" id="feelsLikeDay2">
-            Sunny
-          </div>
-          <div className="col" id="feelsLikeDay3">
-            Sunny
-          </div>
-          <div className="col" id="feelsLikeDay4">
-            Sunny
-          </div>
-          <div className="col" id="feelsLikeDay5">
-            Sunny
-          </div>
-        </div>
-      </div>
-    </div>
   );
 }
